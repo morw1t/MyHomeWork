78714f221f25fcfca4a99dd24130c00e5db24d38
morwit <vitalik.potashev@yandex.ru>
2022-02-27 01:11:48 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkLesson1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HomeWorkLesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как тебя зовут?");
+            string UserName;
+            UserName = Console.ReadLine();
+            string currentDate = DateTime.Now.Date.ToString();
+            Console.WriteLine("Привет, " + UserName + ", сегодня " + currentDate);
+        }
+    }
+}
